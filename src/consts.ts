3a7df720143cfb4b7af7051001b7199a32116783
Antonio <antonio@wolkk.com>
2026-05-22 11:25:03 +0700

    new commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Lumen Studio';
+export const SITE_DESCRIPTION = 'We design digital products that people love.';
